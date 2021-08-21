f99df98c33a0a20cd1be3bc8875fd3ee6199370f
cleveraziko <74561277+cleveraziko@users.noreply.github.com>
2021-08-21 16:11:29 +0500

    finish

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard';
 
 function ImageList(props) {
-  console.log(props);
-  return <div>Image list</div>;
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
+  });
+
+  return <div className='image-list'>{images}</div>;
 }
 
 export default ImageList;
